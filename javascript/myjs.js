0fb65e1b9ba0be931ab39c5a707fe80976c8c64e
Rita Lopes <1170296@isep.ipp.pt>
2019-09-06 16:22:48 +0100

    Bubbles animations

--- javascript/myjs.js
+++ javascript/myjs.js
@@ -26,78 +26,77 @@ function ToggleAnimation(animation){
                 stop2 = true;
                 break;
             default:
-                stop0 = stop1 = stop2 =false;
+                stop0 = stop1 = stop2 =true;
         }
 
         switch(animation){
             case 0:
                 stop0 = false;
-                $('#box-area').html('');
+                $('#anim0 #box-area').html('');
                 RecursiveSquares(stop0);
                 break;
             case 1:
                 stop1 = false;
-                //ManiacDiv(stop1,0);
+                $('#anim1 #box-area').html('');
+                Bubbles(stop1);
                 break;
             case 2:
                 stop2 = false;
                 ConcentricSquares(stop2,0);
                 break;
             default:
-                stop0 = stop1 = stop2 = false;
+                stop0 = stop1 = stop2 = true;
         }
         curAnim = animation;
     }
 }
 
 function RecursiveSquares(stop) {        
-    $('#box-area').append('<li></li>');
-    randomSize = Math.floor(Math.random()*180) + 15;
-    $('#box-area li:last-child').height(randomSize);
-    $('#box-area li:last-child').width(randomSize);
+    $('#anim0 #box-area').append('<li></li>');
+    var randomSize = Math.floor(Math.random()*180) + 15;
+    $('#anim0 #box-area li:last-child').height(randomSize);
+    $('#anim0 #box-area li:last-child').width(randomSize);
     
-    randomPosition = Math.floor(Math.random()*100);
-    $('#box-area li:last-child').css({left:randomPosition + '%'});
+    var randomPosition = Math.floor(Math.random()*100);
+    $('#anim0 #box-area li:last-child').css({left:randomPosition + '%'});
     
-    randomOpacity = Math.floor(Math.random()) + 0.6;
-    $('#box-area li:last-child').css({opacity:randomOpacity});
+    var randomOpacity = Math.floor(Math.random()) + 0.6;
+    $('#anim0 #box-area li:last-child').css({opacity:randomOpacity});
 
-    randomRotation = Math.floor(Math.random()*180);
-    $('#box-area li:last-child').css({transform:'translateY(300%) rotate(' + randomRotation + 'deg)'});
+    var randomRotation = Math.floor(Math.random()*180);
+    $('#anim0 #box-area li:last-child').css({transform:'translateY(300%) rotate(' + randomRotation + 'deg)'});
     
     if(stop){
         return;
     }
-    randomDelay = Math.floor(Math.random()*500);
+    var randomDelay = Math.floor(Math.random()*500);
     setTimeout(() => {
         RecursiveSquares(stop0);
     },randomDelay);
 }
 
-function ManiacDiv(stop) {
-    randomColor = colors[Math.floor(Math.random()*5)];
-    /*
-    random1 = Math.floor(Math.random()*100);
-    random2 = Math.floor(Math.random()*100);
-    random3 = Math.floor(Math.random()*100);
-    random4 = Math.floor(Math.random()*100);
-    random5 = Math.floor(Math.random()*100);
-    random6 = Math.floor(Math.random()*100);
-    random7 = Math.floor(Math.random()*100);
-    random8 = Math.floor(Math.random()*100);
+function Bubbles(stop) {        
+    $('#anim1 #box-area').append('<li></li>');
+    var randomSize = Math.floor(Math.random()*130) + 15;
+    $('#anim1 #box-area li:last-child').height(randomSize);
+    $('#anim1 #box-area li:last-child').width(randomSize);
+    
+    var randomPosition = Math.floor(Math.random()*100);
+    $('#anim1 #box-area li:last-child').css({left:randomPosition + '%'});
+    
+    var randomOpacity = Math.floor(Math.random()) + 0.4;
+    $('#anim1 #box-area li:last-child').css({opacity:randomOpacity});
 
-    $('#anim1 .container').css({'clip-path':'polygon(' + random1 + ' ' + random2 + ', ' + random3 + ' '+ random4 + ', ' + random5 +' ' + random6 +', ' + random7 +' ' + random8 +', 0 0)',
-    '-webkitclip-path':'polygon(' + random1 + ' ' + random2 + ', ' + random3 + ' '+ random4 + ', ' + random5 +' ' + random6 +', ' + random7 +' ' + random8 +', 0 0)',
-    '-moz-clip-path':'polygon(' + random1 + ' ' + random2 + ', ' + random3 + ' '+ random4 + ', ' + random5 +' ' + random6 +', ' + random7 +' ' + random8 +', 0 0)',
-    'background-color':randomColor});
-    */
+    var randomColor = colors[Math.floor(Math.random()*5)];
+    $('#anim1 #box-area li:last-child').css({'background-color':randomColor});
 
     if(stop){
         return;
     }
+    var randomDelay = Math.floor(Math.random()*800) + 100;
     setTimeout(() => {
-        ManiacDiv(stop1);
-    },500);
+        Bubbles(stop1);
+    },randomDelay);
 }
 
 function ConcentricSquares(stop, n) {
